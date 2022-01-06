dd7c1447de676fbdafe792c3eaa3bb86dee3e497
Adam Cohen <acohen89@binghamton.edu>
2022-01-05 17:03:29 -0800

    Got HTTP request to work

--- MedInfo.java
+++ MedInfo.java
@@ -1,5 +1,26 @@
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 class MedInfo {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+
+    public static String get(String ur) throws IOException, InterruptedException{
+        // Create a neat value object to hold the URL
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(ur))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+        return ur;
+    }
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // System.out.println("Hello, World!"); 
+        System.out.println(get("https://www.drugs.com/acetaminophen.html")); 
     }
 }
